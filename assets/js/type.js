4f193704e7227980b80d0b0a5bd08ac80ab18ba4
Igor Savelev <savelevi@mail.ru>
2020-03-29 23:23:55 +0300

    First commit

---
+++ assets/js/type.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  smartBackspace: true,
+  typeSpeed: 40,
+  backDelay: 3000,
+  backSpeed: 40,
+  startDelay: 2500,
+  loop: true,
+  loopCount: Infinity,
+  contentType: "html"
+});
